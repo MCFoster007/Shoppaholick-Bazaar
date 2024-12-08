faf210ca7801e3ce243da6c8011b33041b3293d2
dcruzel <liz.c.dcruz@gmail.com>
2024-12-08 16:55:34 -0500

    add pages, navbar links

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useRef } from "react";
+const Login = () => {
+    return (
+        <>
+            {
+                <div>
+                    <h1>Login Page</h1>
+                </div>    
+            }
+        </>
+    );
+}
+export default Login;
