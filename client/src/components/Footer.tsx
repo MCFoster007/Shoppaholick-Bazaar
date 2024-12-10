c501f2f3c03f2f6a3d53f70dfb3f8ed2e8e4aefa
dcruzel <liz.c.dcruz@gmail.com>
2024-12-09 21:56:09 -0500

    update image folder location and css code

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -13,11 +13,10 @@ const Footer: React.FC = () => {
   }
 
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container text-center mb-5">
+    <footer>
+      <div>
         {location.pathname !== '/' && (
           <button
-            className="btn btn-dark mb-3"
             onClick={handleGoBack}
           >
             &larr; Go Back
